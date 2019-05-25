be70fdb9d38faf34dccf7022d0673665777126a1
Kinga Wnuk <kinga.wnuk@protonmail.com>
2019-05-25 20:22:00 +0200

    Heroku works - postgresql dependency added

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,6 +2,8 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 //public class TasksApplication extends SpringBootServletInitializer {
@@ -15,5 +17,4 @@ public class TasksApplication {
 //	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 //		return application.sources(TasksApplication.class);
 //	}
-
 }
