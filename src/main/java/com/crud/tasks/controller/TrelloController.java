0449d8155f22c0b8f1288ab18693ccd0a820a5eb
Kinga Wnuk <kinga.wnuk@protonmail.com>
2019-05-26 12:12:36 +0200

    added TrelloClient, get request

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,24 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/trello")
+public class TrelloController {
+
+    @Autowired
+    private TrelloClient trelloClient;
+
+    @GetMapping(path = "/tasks")
+    public void getTrelloBoards() {
+        List<TrelloBoardDto> trelloBoardDtos = trelloClient.getTrelloBoards();
+        trelloBoardDtos.forEach(t -> System.out.println(t.getId() + " " + t.getName()));
+    }
+}
