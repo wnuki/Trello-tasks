e6db2f7f5fdbbd8edb8ef0848759e1169b3bcd7b
Kinga Wnuk <kinga.wnuk@protonmail.com>
2019-05-28 16:38:43 +0200

    Simple mail service added

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -2,14 +2,7 @@ package com.crud.tasks.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.validation.MessageCodesResolver;
-import org.springframework.validation.Validator;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.*;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -17,12 +10,9 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.List;
-
-
 @EnableSwagger2
 @Configuration
-public class CoreConfiguration implements WebMvcConfigurer {
+public class CoreConfiguration extends WebMvcConfigurerAdapter{
 
     @Bean
     RestTemplate restTemplate() {
@@ -47,74 +37,4 @@ public class CoreConfiguration implements WebMvcConfigurer {
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
-
-    public void configurePathMatch(PathMatchConfigurer configurer) {
-
-    }
-
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-
-    }
-
-    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
-
-    }
-
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-
-    }
-
-    public void addFormatters(FormatterRegistry registry) {
-
-    }
-
-    public void addInterceptors(InterceptorRegistry registry) {
-
-    }
-
-    public void addCorsMappings(CorsRegistry registry) {
-
-    }
-
-    public void addViewControllers(ViewControllerRegistry registry) {
-
-    }
-
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-
-    }
-
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-
-    }
-
-    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
-
-    }
-
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-
-    }
-
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-
-    }
-
-    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
-
-    }
-
-    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
-
-    }
-
-    public Validator getValidator() {
-        return null;
-    }
-
-    public MessageCodesResolver getMessageCodesResolver() {
-        return null;
-    }
-
-
 }
---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,38 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleEmailServiceTest {
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+
+    @Mock
+    JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmail() {
+        //given
+        Mail mail = new Mail("test@test.com", "Test", "Test message");
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+
+        //when
+        simpleEmailService.send(mail);
+
+        //then
+        verify(javaMailSender, times(1)).send(mailMessage);
+    }
+}
